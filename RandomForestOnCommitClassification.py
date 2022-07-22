f2bb0ab0ad4c3b3c2f91bc2484efcf4bb2701192
Basirul-Islam <bsse1022@iit.du.ac.bd>
2022-07-23 02:36:53 +0600

    Commit message classification

--- RandomForestOnCommitClassification.py
+++ RandomForestOnCommitClassification.py
@@ -3,41 +3,35 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 # Fitting Random Forest Regression to the dataset
 # import the regressor
 from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
-
+from sklearn.model_selection import train_test_split
 # create regressor object
 regressor = RandomForestRegressor(n_estimators=100, random_state=0)
 
 
 
-def print_hi(name):
-    iris = datasets.load_iris()
-    # dividing the datasets into two parts i.e. training datasets and test datasets
-    X, y = datasets.load_iris(return_X_y=True)
+def RandomForestClassification():
+    df = pd.DataFrame(pd.read_excel("DataSet1.xlsx"))
+    print(df.head())
+    print(df.columns)
+
+    X = df.iloc[: , 4:]
+    y = df.iloc[:, 3]
+
 
     # Spliting arrays or matrices into random train and test subsets
-    from sklearn.model_selection import train_test_split
+
     # i.e. 70 % training dataset and 30 % test datasets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
 
 
     # importing random forest classifier from assemble module
     from sklearn.ensemble import RandomForestClassifier
-
-    # creating dataframe of IRIS dataset
-    data = pd.DataFrame({'sepallength': iris.data[:, 0], 'sepalwidth': iris.data[:, 1],
-    'petallength': iris.data[:, 2], 'petalwidth': iris.data[:, 3],
-    'species': iris.target})
-
-
-    # printing the top 5 datasets in iris dataset
-    print(data.head())
 
     # creating a RF classifier
     clf = RandomForestClassifier(n_estimators=100)
@@ -51,21 +45,18 @@ def print_hi(name):
 
     # metrics are used to find accuracy or error
     from sklearn import metrics
-    print()
 
     # using metrics module for accuracy calculation
     print("ACCURACY OF THE MODEL: ", metrics.accuracy_score(y_test, y_pred))
 
     # predicting which type of flower it is.
-    prediction = clf.predict([[2, 1, 4, 5]])
-    print(iris.target_names[prediction])
+    prediction = clf.predict([[1, 1, 0, 0, 1, 2, 0, 0, 0, 0, 0, 1, 2, 3, 4, 0, 0, 0, 0, 1, 0, 1, 1, 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+                               0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 1, 0, 1, 0, 2, 1, 1, 0, 0, 0, 0, 1, 0]])
+    print(prediction)
+
 
 if __name__ == '__main__':
-    #print("hello")
-    #df = pd.read_csv('YoutubeSpamMergedData.csv')
 
-    #print(df.to_string())
-
-    print_hi('Bashir')
+    RandomForestClassification()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
